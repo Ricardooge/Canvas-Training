8896515053753d0a10a9d4cf22c87edee21fccef
Yufei <yufei@MacBook-Pro.local>
2021-11-26 12:18:43 +0800

    phase 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef,useEffect } from 'react';
+
 
 function App() {
+  const [imgSrc, setImage] = useState('')
+  const canvasRef = useRef();
+  const showImage = () =>{
+    setImage('https://media-public.canva.cn/k2Xu0/MAEhC6k2Xu0/1/s2.png');
+  }
+
+  useEffect(() => {
+     //1. 获取canvas元素，用Ref来获取DOM元素
+     const canvas = canvasRef.current;
+     console.log("click",canvas);
+     //2. 获取2D上下文
+     const ctx = canvas.getContext("2d");
+     //3. 新建一个Image对象
+     const img = new Image();
+ 
+     
+     //4. 设置Image的src
+     img.src = imgSrc;
+     //5. 确定Image加载完毕后将Image画到canvas上
+     console.log("imgsrc",img.src)
+     img.onload = () =>{
+       img.width = canvas.width;
+       img.height = canvas.height;
+       ctx.drawImage(img, 0, 0)
+     }
+  },[imgSrc])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas id="canvas"
+      ref={canvasRef}
+      width = {360}
+      height = {640}
+     
+    />
+    <button onClick={showImage}>Start</button>
+
+   
     </div>
   );
 }
